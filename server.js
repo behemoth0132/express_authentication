4d9ff31468886ea3042384b098db82ed116735a1
jwright1992 <jordanwright1992@gmail.com>
2021-05-13 12:29:01 -0700

    isLoggedIn: add middleware and import to server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('./config/ppConfig');
 const SECRET_SESSION = process.env.SECRET_SESSION;
-
+const isLoggedIn = require('./middleware/isLoggedIn');
 
 app.set('view engine', 'ejs');
 
@@ -15,15 +15,11 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
-app.use(passport.initialize());      // Initialize passport
-app.use(passport.session());         // Add a session
-
 app.use(session({
   secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
   resave: false,             // Save the session even if it's modified, make this false
   saveUninitialized: true    // If we have a new session, we save it, therefore making that true
 }));
-
 app.use(flash());    
         // flash middleware
 app.use((req, res, next) => {
@@ -32,6 +28,9 @@ app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   next();
 });
+app.use(passport.initialize());      // Initialize passport
+app.use(passport.session());         // Add a session
+
 app.get('/', (req, res) => {
   res.render('index');
 });
