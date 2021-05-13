874fa40540e4c7b53bba9569ee3e9a5e17461364
jwright1992 <jordanwright1992@gmail.com>
2021-05-13 12:10:29 -0700

    server: Import passport and pass through middleware

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ const layouts = require('express-ejs-layouts');
 const app = express();
 const session = require('express-session');
 const flash = require('connect-flash');
-
+const passport = require('./config/ppConfig');
 const SECRET_SESSION = process.env.SECRET_SESSION;
 
 
 app.set('view engine', 'ejs');
 
+
 app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+app.use(passport.initialize());      // Initialize passport
+app.use(passport.session());         // Add a session
 
 app.use(session({
   secret: SECRET_SESSION,    // What we actually will be giving the user on our site as a session cookie
